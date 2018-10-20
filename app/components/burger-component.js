443c03721ad69e17af86658760e156998a0b6071
sklinov <sergeyklinov@gmail.com>
2018-10-20 12:00:47 +0500

    HW5
    Menu added

--- app/components/burger-component.js
+++ app/components/burger-component.js
@@ -1,13 +1,12 @@
 angular.
 module('newsApp').
-component('menubar', {
-    templateUrl: 'components/menu-component-template.html',
+component('burger', {
+    templateUrl: "components/burger-component-template.html",
     controller:
         ['$http', function MenuItemsContr($http) {
             var self = this;
             $http.get('data/menu.json').then(function(response){
                 self.menuitem=response.data;
-            })
-        }]
-
+            });
+    }]
 })
